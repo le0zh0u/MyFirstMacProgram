8fc5ac12034005a64777ca5e4a387996b4a6c567
leozhou <492753662@qq.com>
2017-12-10 18:41:41 +0800

    add search text field, token field, secure field

--- MyFirstProgram/ViewController.swift
+++ MyFirstProgram/ViewController.swift
@@ -9,6 +9,16 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var searchBox: NSSearchField!
+    @IBOutlet weak var historyBox: NSTextField!
+    
+    @IBOutlet weak var tokenBox: NSTokenField!
+    @IBOutlet weak var tokenResult: NSTextField!
+    
+    @IBOutlet weak var secureBox: NSSecureTextField!
+    @IBOutlet weak var secureResults: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +29,16 @@ class ViewController: NSViewController {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    @IBAction func showHistory(_ sender: NSButton) {
+        historyBox.stringValue = String(stringInterpolationSegment: searchBox.recentSearches)
+    }
+    @IBAction func showToken(_ sender: NSButton) {
+        tokenResult.stringValue = tokenBox.stringValue
+    }
+    @IBAction func showSecret(_ sender: NSButton) {
+        secureResults.stringValue = secureBox.stringValue
     }
 }
 
