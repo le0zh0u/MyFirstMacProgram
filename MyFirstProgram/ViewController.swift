e56bb9fc1da54d8e313e28a156f4165d7b772cad
leozhou <492753662@qq.com>
2017-12-11 23:27:08 +0800

    add alert program

--- MyFirstProgram/ViewController.swift
+++ MyFirstProgram/ViewController.swift
@@ -22,6 +22,57 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    @IBAction func shouAlert(_ sender: NSButton) {
+        let myAlert = NSAlert()
+        myAlert.messageText = "Warning!"
+        myAlert.informativeText = "Zombies approaching"
+        myAlert.alertStyle = NSAlertStyle.critical
+        myAlert.showsSuppressionButton = true
+        myAlert.suppressionButton?.title = "Stop scaring me"
+        
+        myAlert.addButton(withTitle: "Ignore it")
+        myAlert.addButton(withTitle: "Run")
+        myAlert.addButton(withTitle: "panic")
+        myAlert.addButton(withTitle: "Do nothing")
+        
+//        let choice = myAlert.runModal()
+//        
+//        switch choice {
+//        case NSAlertFirstButtonReturn:
+//            print ("User clicked Ignore it")
+//        case NSAlertSecondButtonReturn:
+//            print ("User clicked Run")
+//        case NSAlertThirdButtonReturn:
+//            print ("User clicked Panic")
+//        case NSAlertThirdButtonReturn + 1:
+//            print ("User clicked Do nothing")
+//        default: break
+//        }
+//        if myAlert.suppressionButton!.state == 1 {
+//            print ("Checked")
+//        } else {
+//            print ("Not checked")
+//        }
+        
+        let myCode = { (choice:NSModalResponse) -> Void in
+            switch choice {
+            case NSAlertFirstButtonReturn:
+                print ("User clicked Ignore it")
+            case NSAlertSecondButtonReturn:
+                print ("User clicked Run")
+            case NSAlertThirdButtonReturn:
+                print ("User clicked Panic")
+            case NSAlertThirdButtonReturn + 1:
+                print ("User clicked Do nothing")
+            default: break
+            }
+            if myAlert.suppressionButton!.state == 1 {
+                print ("Checked")
+            } else {
+                print ("Not checked")
+            } }
+        myAlert.beginSheetModal(for: NSApp.keyWindow!, completionHandler: myCode)
+    }
     
 }
 
